11346dbd257be032aba5826281dbca951c4efced
Alexander Suhoverhov <cy@ngs.ru>
2013-12-25 19:37:41 +0400

    Fix bug with cleared screen on first getch

--- examples/hello.lisp
+++ examples/hello.lisp
@@ -3,8 +3,8 @@
 
 (defun main ()
   (cl-tui:init-screen)
-  (cl-tui:read-key)
   (cl-tui:put-text :root 0 0 "Hello world!")
+  (cl-tui:put-text :root 1 2 "Press any key...")
   (cl-tui:refresh)
   (cl-tui:read-key)
   (cl-tui:destroy-screen))
