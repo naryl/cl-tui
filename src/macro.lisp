9405ff9be2e45c971abd0c9c808ff69939c2759d
Alexander Suhoverhov <cy@ngs.ru>
2014-03-27 19:52:30 +0400

    Don't teach bad things
    --HG--
    branch : dev

--- src/macro.lisp
+++ src/macro.lisp
@@ -6,10 +6,11 @@
 (defmacro when-running (&body body)
   (with-gensyms (func)
     `(labels ((,func () ,@body))
-       (when *running*
-         (,func))
-       (push #',func *delayed-init*)
+       (if *running*
+           (,func)
+           (push #',func *delayed-init*))
        nil)))
 
 (defun do-delayed-init ()
-  (mapc #'funcall (nreverse *delayed-init*)))
+  (mapc #'funcall (nreverse *delayed-init*))
+  (setf *delayed-init* nil))
